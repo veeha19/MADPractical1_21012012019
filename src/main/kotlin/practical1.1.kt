878e49ab7dd8080db3c68d6f47df4cf4478add54
SONY <patelarpit521@gmail.com>
2022-08-13 10:28:53 +0530

    Initial commit

---
+++ src/main/kotlin/practical1.1.kt
@@ -0,0 +1,23 @@
+
+fun main(){
+
+    val a:Int=42
+    val b:Float=45.45f
+    val c:Double=45.67
+    val d:Boolean=false
+    val e:Long=23265248
+    val f:Short=-8
+    val g:Byte=45
+    val h: Char ='A'
+    val i:String="AMP"
+    println("Integer value : $a")
+    println("Float value : $b")
+    println("Double value : $c")
+    println("Boolean value : $d")
+    println("Long value : $e")
+    println("Short value : $f")
+    println("Byte value : $g")
+    println("Char value : $h")
+    println("String value : $i")
+
+}
