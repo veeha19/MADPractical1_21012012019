c9ff9689674564c13a54831f7630b5280cd47557
SONY <patelarpit521@gmail.com>
2022-08-15 19:40:41 +0530

    practical1.4.kt

---
+++ src/main/kotlin/practical1.4.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+    print("Enter the Number:")
+    val a= readLine()!!.toInt()
+    if(a%2==0){
+        println("$a is even")
+    }
+    else{
+        println("$a is odd")
+
+    }
+}
