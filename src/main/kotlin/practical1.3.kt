3247bb2eb5458792bcaadc0392384a9a2a44442e
SONY <patelarpit521@gmail.com>
2022-08-15 19:40:00 +0530

    practical1.2.kt

---
+++ src/main/kotlin/practical1.3.kt
@@ -0,0 +1,29 @@
+fun main (){
+    print("Student Enrollment no:")
+    val a= readLine()
+    print("Student name:")
+    val b= readLine()
+    print("Student Branch:")
+    val c= readLine()
+    print("Student Class:")
+    val d= readLine()
+    print("Student Collage Name:")
+    val e= readLine()
+    print("Student University Name:")
+    val f= readLine()
+    print("Student Age:")
+    val g= readLine()
+
+    println("/n/n********************/n/n")
+
+    println("Student Data:")
+    println("Enrollment:$a")
+    println("Name:$b")
+    println("Branch:$c")
+    println("Class:$d")
+    println("Collage Name:$e")
+    println("University Name:$f")
+    println("Age:$g")
+
+
+}
