cda58972831b14b7c98efc242be50dcdef37bd21
SONY <patelarpit521@gmail.com>
2022-08-19 18:44:48 +0530

    practical 2

---
+++ src/main/kotlin/practical 2.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+
+    val a: Int = 20
+    val b: Double =a.toDouble()
+    val c: String = "10"
+    val d: Int = c.toInt()
+    val e: String = "11.23"
+    val f: Double = e.toDouble()
+    println("Integer Value : $a")
+    println("Double Value (from Integer) : $b")
+    println("Integer value :$c")
+    println("Double value (from Integer) :$f")
+
+}
