2799720d4c8885da23a5a3c6f441a0d22d3e1d29
SONY <patelarpit521@gmail.com>
2022-08-21 17:25:28 +0530

    practical 7

---
+++ src/main/kotlin/practical 7.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    print("Enter number: ")
+    val number = readLine()!!.toInt()
+    val factorial = fact(number)
+    println("Factorial of $number = $factorial")
+    println("By Tailrec Keyword,Factorial of $number = $factorial")
+
+}
+tailrec fun fact(n: Int, temp: Int = 1): Int
+{
+    return if (n == 1)
+    {
+        temp
+    } else {
+        fact(n-1, temp*n)
+    }
+}
